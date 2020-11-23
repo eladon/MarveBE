1f003bdceee43a0e7d84c5892cdd0c737c7d666c
Elad Zicherman <eladonz@gmail.com>
2020-11-23 17:52:14 +0200

    Added dotenv

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const errorHandler = require('_helpers/error-handler');
+const dotenv = require('dotenv');
 
 app.use(helmet());
+dotenv.config();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
@@ -19,7 +21,10 @@ app.use('/actors', require('./actors/actors.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+const port =
+  process.env.NODE_ENV === 'production'
+    ? process.env.PORT || 443
+    : 4000;
 const server = app.listen(port, function () {
   console.log('Server listening on port ' + port);
 });
